a8e4816470915cd099cc1a24f1ae3ed0da7b7184
Jonathan Chaney <jonchaney@gmail.com>
2017-09-21 14:48:15 -0700

    miniMaxSum solved

--- javascript/miniMaxSum.js
+++ javascript/miniMaxSum.js
@@ -10,30 +10,16 @@
 // Sample Output
 // 10 14
 
-// 1) create sub array of all but one integer
-// 2) sum the sub array
-// 3) test if number is greater than max variable, if so reassign
-// 4) check if variable is less than min variable, if so reassign
-// 5) return min and max variables
-
-function miniMaxSum(arr) {
-  let max = 0;
-  let min = arr[0] + arr[1] + arr[2] + arr[3];
-  for(let i = 0; i < arr.length; i++) {
-    let sub_arr = arr.slice(0);
-    sub_arr.splice(i,1);
-    let sum = sub_arr.reduce(function(acc, curVal){
-      return acc + curVal;
-    });
-    if (sum > max) {
-      max = sum;
-    } else if (sum < min) {
-      min = sum;
-    }
-  }
-  return [min,max];
+function sum(x, y) {
+  return Number(x) + Number(y);
 }
 
-let res = miniMaxSum([1,2,3,4,5]);
+function main() {
+  var arr = [1,2,3,4,5];
+  const sorted = arr.sort();
+  const min = sorted.slice(0, 4).reduce(sum, 0);
+  const max = sorted.slice(1, 5).reduce(sum, 0);
+  console.log(min, max);
+}
 
-console.log(res[0] + " " + res[1]);
+main();
